e9a0620d7a729d30d223bb9142ffac00784b5454
Delfina <delfinapolito@gmail.com>
2015-07-07 12:21:05 -0300

    utils and turismoApp.js

--- js/components/DexApp.react.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-var DexApp = React.createClass({
-  render: function() {
-    return(<div>Hello Dex</div>)
-  }
-});
-
-module.exports = DexApp;
---
+++ js/components/TurismoApp.react.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var TurismoApp = React.createClass({
+  render: function() {
+    return(<div>Turismo</div>)
+  }
+});
+
+module.exports = TurismoApp;
