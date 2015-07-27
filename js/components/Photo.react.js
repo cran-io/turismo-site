d74973d22c5fb87710d62f3177cbacf843af2bc4
Maikol <miguel.deelias@gmail.com>
2015-07-27 20:49:26 -0300

    facebook share

--- js/components/Photo.react.js
+++ js/components/Photo.react.js
@@ -7,27 +7,11 @@ module.exports = React.createClass({
   },
 
   componentDidMount: function() {
-    if (window.twttr) {
-      window.twttr.widgets.load()
-    } else {
-      window.twttr = (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            t = window.twttr || {};
-        if (d.getElementById(id)) return t;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://platform.twitter.com/widgets.js";
-        fjs.parentNode.insertBefore(js, fjs);
+    this.facebookShare();
+    this.twitterShare();
+  },
 
-        t._e = [];
-        t.ready = function(f) {
-          t._e.push(f);
-        };
-
-        return t;
-      }(document, "script", "twitter-wjs"));
-    }
-
+  facebookShare: function() {
     if (window.fbAsyncInit) {
       window.fbAsyncInit();
     } else {
@@ -47,6 +31,29 @@ module.exports = React.createClass({
       js.src = "//connect.facebook.net/en_US/sdk.js";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
+  },
+
+  twitterShare: function() {
+    if (window.twttr) {
+      window.twttr.widgets.load()
+    } else {
+      window.twttr = (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            t = window.twttr || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+
+        t._e = [];
+        t.ready = function(f) {
+          t._e.push(f);
+        };
+
+        return t;
+      }(document, "script", "twitter-wjs")); 
+    }
   },
 
   componentWillUnmount: function() {
