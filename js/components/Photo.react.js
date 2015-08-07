481fb6b1d771012f4c74ff02e426db4d47d82cae
Maikol <miguel.deelias@gmail.com>
2015-08-07 16:59:45 -0300

    urls in shares

--- js/components/Photo.react.js
+++ js/components/Photo.react.js
@@ -79,8 +79,8 @@ module.exports = React.createClass({
                   <div className="row">
                       <div className="col-lg-offset-3 col-lg-6 col-xs-offset-2 col-xs-8 center-align">
                           <p className="pShare">COMPARTIR EN:</p>
-                          <div className="fb-share-button fbMargin" data-href="http://sesorium.tecnolopolis.argentina.tur.ar/" data-layout="button"></div>
-                          <a href="https://twitter.com/intent/tweet?url=http://sesorium.tecnolopolis.argentina.tur.ar/" className="twitter-share-button fbMargin" data-size="default" data-count="none"></a>
+                          <div className="fb-share-button fbMargin" data-href="http://sensorium.tecnopolis.argentina.tur.ar/" data-layout="button"></div>
+                          <a href="https://twitter.com/intent/tweet?url=http://sensorium.tecnopolis.argentina.tur.ar/" className="twitter-share-button fbMargin" data-size="default" data-count="none"></a>
 
                       </div>
                       <div className="col-lg-12 col-xs-12 center-align downloadMargin">
